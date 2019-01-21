33e51068abbde7d9da6c034863658dc0e1985703
rotaliator <rotaliator@gmail.com>
2019-01-22 00:06:58 +0100

    pure python mandelbrot

---
+++ profract/core.py
@@ -0,0 +1,45 @@
+from itertools import product, repeat
+from functools import partial
+from array import array
+from PIL import Image
+
+IMAGE_WIDTH = 800
+IMAGE_HEIGHT = 600
+
+def scale(comp_size, pixel_size, pixel):
+    return pixel*comp_size / pixel_size
+
+def mandel_for(re, im, max_dist=2**6, max_iter=255):
+    z_re, z_im = re, im
+    for i in range(max_iter):
+        z_re, z_im = (z_re*z_re - z_im*z_im + re, 2*z_re*z_im + im)
+        if ((z_re*z_re + z_im*z_im) >= max_dist):
+            return i
+    return max_iter
+
+def mandel(re1, im1, re2, im2, width, height):
+    """
+    Calculates iterations of mandelbrot set into array of floats
+    pure Python reference implementation
+    re1, im1 - upper left corner
+    re2, im2 - lower right corner
+    width, height - size of output image
+    """
+    a = array('B', repeat(255, width*height))
+    comp_size_re = re2 - re1
+    comp_size_im = im2 - im1
+    scale_re = partial(scale, comp_size_re, width)
+    scale_im = partial(scale, comp_size_im, height)
+    for y, x in product(range(height), range(width)):
+        index = x + y*width
+        a[index] = mandel_for(re1+scale_re(x), im1+scale_im(y))
+    return a
+
+
+def main():
+    m = mandel(-2.0, -1.0, 1.0, 1.0, IMAGE_WIDTH, IMAGE_HEIGHT)
+    i = Image.frombytes(mode='L', data=bytes(m), size=(IMAGE_WIDTH, IMAGE_HEIGHT))
+    i.save('mandel.png')
+
+if __name__ == '__main__':
+    main()
