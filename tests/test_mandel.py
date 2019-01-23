3c5ae0570539a0445430b5d7b8861e8612752194
rotaliator <rotaliator@gmail.com>
2019-01-23 21:52:39 +0100

    tests

---
+++ tests/test_mandel.py
@@ -0,0 +1,11 @@
+import pytest
+from profract.mandel.pure_python import mandel_for
+
+@pytest.mark.parametrize("re, im, expected", [
+    (1.0, 1.0, 1),
+    (1.0, -1.0, 1),
+    (0.0, 0.0, 255),
+    (-1.0, 0.0, 255),
+])
+def test_mandel_for(re, im, expected):
+    assert mandel_for(re, im, max_iter=255) == expected
