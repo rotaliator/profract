63fbc1b13df0f1640470e5b7746be6541b76c8ea
rotaliator <rotaliator@gmail.com>
2019-01-24 22:26:21 +0100

    initial cython version
    compile with: python setup.py build_ext --inplace

---
+++ profract/mandel/cython.pyx
@@ -0,0 +1,53 @@
+from itertools import repeat, product
+from functools import partial
+from cpython cimport array
+
+def scale(comp_size, pixel_size, pixel):
+    return pixel*comp_size / pixel_size
+
+cdef int mandel_for(float re, float im, double max_dist=2**6,int max_iter=255):
+    z_re, z_im = re, im
+    for i in range(max_iter):
+        z_re, z_im = z_re*z_re - z_im*z_im + re, 2*z_re*z_im + im
+        if ((z_re*z_re + z_im*z_im) >= max_dist):
+            return i
+    return max_iter
+
+def mandel_classic(re1, im1, re2, im2, width, height):
+    """
+    Calculates iterations of mandelbrot set into array of floats
+    pure Python reference implementation
+    re1, im1 - upper left corner
+    re2, im2 - lower right corner
+    width, height - size of output image
+    """
+    a = array.array('B', repeat(255, width*height))
+    comp_size_re = re2 - re1
+    comp_size_im = im2 - im1
+    scale_re = partial(scale, comp_size_re, width)
+    scale_im = partial(scale, comp_size_im, height)
+    for y, x in product(range(height), range(width)):
+        index = x + y*width
+        a[index] = mandel_for(re1+scale_re(x), im1+scale_im(y))
+    return a
+
+def f(re_im):
+    re, im = re_im
+    return mandel_for(re, im)
+
+def mandel_cython(re1, im1, re2, im2, width, height):
+    """
+    Calculates iterations of mandelbrot set into array of floats
+    pure Python reference implementation
+    re1, im1 - upper left corner
+    re2, im2 - lower right corner
+    width, height - size of output image
+    """
+    comp_size_re = re2 - re1
+    comp_size_im = im2 - im1
+    scale_re = partial(scale, comp_size_re, width)
+    scale_im = partial(scale, comp_size_im, height)
+    pixels = product(range(height), range(width))
+    points = ((re1+scale_re(x), im1+scale_im(y)) for y, x in pixels)
+
+    return array.array('B', map(f, points))
