910b2c8a2354b4e13a0e66375847fec9f4942eaa
rotaliator <rotaliator@gmail.com>
2019-01-25 08:39:00 +0100

    cython language level set to 3

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules=cythonize("profract/mandel/cython.pyx"),
+    ext_modules=cythonize("profract/mandel/cython.pyx",
+                          compiler_directives={"language_level": 3}),
 )
