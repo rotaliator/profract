ca5f3cbb8e1a8e2391a0da0685a942c56d41e5bb
rotaliator <rotaliator@gmail.com>
2019-01-25 15:16:54 +0100

    float->double

--- profract/mandel/cython.pyx
+++ profract/mandel/cython.pyx
@@ -5,12 +5,12 @@ from cpython cimport array
 
 
 @cython.cdivision(True)
-cdef float scale(float comp_size, int pixels, int pixel) except *:
+cdef double scale(double comp_size, int pixels, int pixel) except *:
     return pixel*comp_size / pixels
 
-cdef unsigned char mandel_for(float re, float im, int max_dist=2**6, int max_iter=255):
-    cdef float z_re = re
-    cdef float z_im = im
+cdef unsigned char mandel_for(double re, double im, int max_dist=2**6, int max_iter=255):
+    cdef double z_re = re
+    cdef double z_im = im
     for i in range(max_iter):
         z_re, z_im = z_re*z_re - z_im*z_im + re, 2*z_re*z_im + im
         if ((z_re*z_re + z_im*z_im) >= max_dist):
@@ -18,17 +18,17 @@ cdef unsigned char mandel_for(float re, float im, int max_dist=2**6, int max_ite
     return max_iter
 
 @cython.boundscheck(False)
-cpdef unsigned char[:] mandel_cython(float re1, float im1, float re2, float im2, int width, int height):
+cpdef unsigned char[:] mandel_cython(double re1, double im1, double re2, double im2, int width, int height):
     """
-    Calculates iterations of mandelbrot set into array of floats
+    Calculates iterations of mandelbrot set into array of doubles
     Cython optimized implementation
     re1, im1 - upper left corner
     re2, im2 - lower right corner
     width, height - size of output image
     """
     cdef int size = width*height
-    cdef float comp_size_re = re2 - re1
-    cdef float comp_size_im = im2 - im1
+    cdef double comp_size_re = re2 - re1
+    cdef double comp_size_im = im2 - im1
     cdef unsigned char[:] a = cython.view.array(shape=(size,), itemsize=sizeof(unsigned char), format='B')
 
     cdef int x = 0
