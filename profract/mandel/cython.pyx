38fa34a873c103ebea138db5a9a6948a3f7b1ff0
rotaliator <rotaliator@gmail.com>
2019-01-25 08:39:26 +0100

    mandel_cython annotated and simplified for better compilation

--- profract/mandel/cython.pyx
+++ profract/mandel/cython.pyx
@@ -1,19 +1,21 @@
-from itertools import repeat, product
-from functools import partial
+from itertools import repeat
 from cpython cimport array
+import array
 
-def scale(comp_size, pixel_size, pixel):
-    return pixel*comp_size / pixel_size
 
-cdef int mandel_for(float re, float im, double max_dist=2**6,int max_iter=255):
-    z_re, z_im = re, im
+cdef float scale(float comp_size, int pixels, int pixel):
+    return pixel*comp_size / pixels
+
+cdef unsigned char mandel_for(float re, float im, int max_dist=2**6, int max_iter=255):
+    cdef float z_re = re
+    cdef float z_im = im
     for i in range(max_iter):
         z_re, z_im = z_re*z_re - z_im*z_im + re, 2*z_re*z_im + im
         if ((z_re*z_re + z_im*z_im) >= max_dist):
             return i
     return max_iter
 
-def mandel_classic(re1, im1, re2, im2, width, height):
+cpdef unsigned char[:] mandel_cython(float re1, float im1, float re2, float im2, int width, int height):
     """
     Calculates iterations of mandelbrot set into array of floats
     pure Python reference implementation
@@ -21,33 +23,20 @@ def mandel_classic(re1, im1, re2, im2, width, height):
     re2, im2 - lower right corner
     width, height - size of output image
     """
-    a = array.array('B', repeat(255, width*height))
-    comp_size_re = re2 - re1
-    comp_size_im = im2 - im1
-    scale_re = partial(scale, comp_size_re, width)
-    scale_im = partial(scale, comp_size_im, height)
-    for y, x in product(range(height), range(width)):
-        index = x + y*width
-        a[index] = mandel_for(re1+scale_re(x), im1+scale_im(y))
+    cdef float comp_size_re = re2 - re1
+    cdef float comp_size_im = im2 - im1
+    cdef array.array a = array.array('B', repeat(255, width*height))
+
+    cdef int x = 0
+    cdef int y = 0
+    cdef int index = 0
+
+    while y < height:
+        x = 0
+        while x < width:
+            index = x + y*width
+            r = mandel_for(re1+scale(comp_size_re, width, x), im1+scale(comp_size_im, height, y))
+            a[index] = r
+            x = x + 1
+        y = y + 1
     return a
-
-def f(re_im):
-    re, im = re_im
-    return mandel_for(re, im)
-
-def mandel_cython(re1, im1, re2, im2, width, height):
-    """
-    Calculates iterations of mandelbrot set into array of floats
-    pure Python reference implementation
-    re1, im1 - upper left corner
-    re2, im2 - lower right corner
-    width, height - size of output image
-    """
-    comp_size_re = re2 - re1
-    comp_size_im = im2 - im1
-    scale_re = partial(scale, comp_size_re, width)
-    scale_im = partial(scale, comp_size_im, height)
-    pixels = product(range(height), range(width))
-    points = ((re1+scale_re(x), im1+scale_im(y)) for y, x in pixels)
-
-    return array.array('B', map(f, points))
