6fa92997dd30f581cb5e4b81c9f36a658ca2a879
rotaliator <rotaliator@gmail.com>
2019-01-25 15:11:05 +0100

    mandel_cython test for larger region

--- tests/test_cython.py
+++ tests/test_cython.py
@@ -10,3 +10,10 @@ from profract.mandel.cython import mandel_cython
 def test_mandel_cython(re, im, expected):
     result = mandel_cython(re, im, re, im, 1, 1)
     assert result[0] == expected
+
+def test_for_profiling():
+    result = mandel_cython(-2.0, -1.0, 1.0, 1.0, 1600, 1200)
+    result_len = len(result)
+    assert result_len == 1600*1200
+    assert result[0] == 1
+    assert result[result_len-1] == 1
