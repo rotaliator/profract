14e58fb310ea79c03e8416c0992ef028acddd8d3
rotaliator <rotaliator@gmail.com>
2019-01-28 10:37:53 +0100

    test for cython multiproc

--- tests/test_cython.py
+++ tests/test_cython.py
@@ -1,5 +1,5 @@
 import pytest
-from profract.mandel.cython import mandel_cython
+from profract.mandel.cython import mandel_cython, mandel_cython_multiproc
 
 @pytest.mark.parametrize("re, im, expected", [
     (1.0, 1.0, 1),
@@ -9,6 +9,17 @@ from profract.mandel.cython import mandel_cython
 ])
 def test_mandel_cython(re, im, expected):
     result = mandel_cython(re, im, re, im, 1, 1)
+    assert result[0] == expected
+
+
+@pytest.mark.parametrize("re, im, expected", [
+    (1.0, 1.0, 1),
+    (1.0, -1.0, 1),
+    (0.0, 0.0, 255),
+    (-1.0, 0.0, 255),
+])
+def test_mandel_cython_multiproc(re, im, expected):
+    result = mandel_cython_multiproc(re, im, re, im, 1, 1)
     assert result[0] == expected
 
 def test_for_profiling():
