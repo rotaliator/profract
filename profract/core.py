b19843d547af399e1db240333f15220dd33025d5
rotaliator <rotaliator@gmail.com>
2019-01-30 13:12:10 +0100

    added palettes (from bokeh project) for better visuals

--- profract/core.py
+++ profract/core.py
@@ -2,6 +2,8 @@ from __future__ import print_function
 import sys
 import time
 import png
+from palette import pypngpalettes
+
 
 IMAGE_WIDTH = 800
 IMAGE_HEIGHT = 600
@@ -17,24 +19,37 @@ class Timer:
         self.interval = time.time() - self.start
         return self
 
-def save_array_as_png(array, filename, width, height):
-    writer = png.Writer(width=width, height=height, greyscale=True)
+def save_array_as_png(array, filename, width, height, palette=None):
+    if palette:
+        writer = png.Writer(width=width, height=height, palette=palette)
+    else:
+        writer = png.Writer(width=width, height=height, greyscale=True)
     with open(filename, "wb") as f:
         writer.write_array(f, array)
 
 def main():
+    viridis256 = pypngpalettes['PAL_Viridis256']
+
     from mandel.pure_python import mandel_classic
     with Timer() as t:
         m = mandel_classic(-2.0, -1.0, 1.0, 1.0, IMAGE_WIDTH, IMAGE_HEIGHT)
     print("Single proc calculations took: {:.2f} sec".format( t.interval))
-    save_array_as_png(m, "mandel_classic.png", IMAGE_WIDTH, IMAGE_HEIGHT)
+
+    # Saving images for all palettes in pypngpalettes map
+    for palname, palette in pypngpalettes.items():
+        save_array_as_png(
+            m,
+            "mandel_classic_{}.png".format(palname.lower()),
+            IMAGE_WIDTH, IMAGE_HEIGHT,
+            palette=palette,
+        )
 
     try:
         from mandel.pure_python_multiproc import mandel as mandel_multiproc
         with Timer() as t:
             m = mandel_multiproc(-2.0, -1.0, 1.0, 1.0, IMAGE_WIDTH, IMAGE_HEIGHT)
         print("Multi proc calculations took: {:.2f} sec".format( t.interval))
-        save_array_as_png(m, "mandel_multiproc.png", IMAGE_WIDTH, IMAGE_HEIGHT)
+        save_array_as_png(m, "mandel_multiproc.png", IMAGE_WIDTH, IMAGE_HEIGHT, palette=viridis256)
     except ImportError as e:
         print(e)
 
@@ -43,7 +58,7 @@ def main():
         with Timer() as t:
             m = mandel_cython(-2.0, -1.0, 1.0, 1.0, IMAGE_WIDTH, IMAGE_HEIGHT)
         print("Cython calculations took: {:.2f} sec".format( t.interval))
-        save_array_as_png(m, "mandel_cython.png", IMAGE_WIDTH, IMAGE_HEIGHT)
+        save_array_as_png(m, "mandel_cython.png", IMAGE_WIDTH, IMAGE_HEIGHT, palette=viridis256)
     except ImportError as e:
         print(e)
 
@@ -52,7 +67,7 @@ def main():
         with Timer() as t:
             m = mandel_cython_multiproc(-2.0, -1.0, 1.0, 1.0, IMAGE_WIDTH, IMAGE_HEIGHT)
         print("Cython multiproc calculations took: {:.2f} sec".format( t.interval))
-        save_array_as_png(m, "mandel_cython_multiproc.png", IMAGE_WIDTH, IMAGE_HEIGHT)
+        save_array_as_png(m, "mandel_cython_multiproc.png", IMAGE_WIDTH, IMAGE_HEIGHT, palette=viridis256)
     except ImportError as e:
         print(e)
 
